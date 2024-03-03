e8f96c5e3b5d1ee0a24da2750c43ba5ce68012fd
Benoit Favier <benoitfamillefavier@gmail.com>
2024-03-03 12:42:01 +0100

    added compilation script

--- src/RenderEngine/graphics/shaders/compile.py
+++ src/RenderEngine/graphics/shaders/compile.py
@@ -2,30 +2,34 @@ import os
 import re
 import pathlib
 import subprocess
+from typing import Iterable
 
 GLSLC = "glslc.exe"
 PATH = pathlib.Path(__file__).parent
 STAGES = {".vert": "VK_SHADER_STAGE_VERTEX_BIT",
           ".frag": "VK_SHADER_STAGE_FRAGMENT_BIT",
           ".comp": "VK_SHADER_STAGE_COMPUTE_BIT"}
-TYPES = {"sampler": "VK_DESCRIPTOR_TYPE_SAMPLER",
-         "sampler2D": "VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER"}
-# regex for bindings "layout([set={0}, ]binding={0}[, offset={0}]) uniform {vec3|image2D|sampler2D|...} {name};"
-BINDING_REGEX = re.compile(r"layout *\((?:set *= *(\d+), *)?binding *= *(\d+)(?:, *offset *= *(\d+))?(?:, *std\d+)?\) *uniform *(\w+)?(?:\s*{[^}]+})? *(\w+)(?:\[(\d+)\])?;")
+UNIFORM_TYPES = {"sampler": "VK_DESCRIPTOR_TYPE_SAMPLER",
+                 "sampler2D": "VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER",
+                 "subpassInput": "VK_DESCRIPTOR_TYPE_INPUT_ATTACHMENT",
+                 "UniformBufferObject": "VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER"}
+BUFFER_TYPE = "VK_DESCRIPTOR_TYPE_STORAGE_BUFFER"
+LAYOUT_REGEX = re.compile(r"layout *\((?:(?:input_attachment_index *= *(\d+))|(?:set *= *(\d+))|(?:binding *= *(\d+))|(?:offset *= *(\d+))|(?:location *= *(\d+))|(?:std\d+)|(?:, *))+\) *(in|out|uniform|buffer) +(\w+)(?:\s*{[^}]+})? *(\w+)?(?:\[(\d+)\])?;")
 
 
 CONSTRUCTOR_SRC = """#include <RenderEngine/graphics/shaders/{shader_name}.hpp>
-#include <RenderEngine/graphics/GPU.hpp>
 using namespace RenderEngine;
 
-std::vector<std::vector<std::vector<std::pair<std::string, VkDescriptorSetLayoutBinding>>>> bindings_sets = {bindings_sets};
-std::vector<std::vector<std::pair<VkVertexInputBindingDescription, VkVertexInputAttributeDescription>>> vertex_inputs = {vertex_inputs};
-std::vector<std::vector<std::pair<std::string, Shader::Type>>> fragment_inputs = {fragment_inputs};
-std::vector<std::vector<std::pair<std::string, Shader::Type>>> fragment_outputs = {fragment_outputs};
+std::vector<std::vector<std::pair<std::string, VkVertexInputBindingDescription>>> vertex_buffer_bindings = {vertex_buffer_bindings};
+std::vector<std::vector<std::pair<std::string, VkVertexInputAttributeDescription>>> vertex_buffer_attributes = {vertex_buffer_attributes};
+std::vector<std::pair<std::string, Type>> attachments = {attachments};
+std::vector<std::vector<std::string>> input_attachments = {input_attachments};
+std::vector<std::vector<std::string>> output_attachments = {output_attachments};
+std::vector<std::vector<std::vector<std::pair<std::string, VkDescriptorSetLayoutBinding>>>> descriptor_sets = {descriptor_sets};
 std::vector<std::vector<std::pair<VkShaderStageFlagBits, std::vector<uint8_t>>>> stages_bytecode = {stages_bytecode};
 
 
-{shader_name}::{shader_name}(const GPU* gpu) : Shader(gpu, bindings_sets, vertex_inputs, fragment_inputs, fragment_outputs, stages_bytecode)
+{shader_name}::{shader_name}(const GPU* gpu) : Shader(gpu, vertex_buffer_bindings, vertex_buffer_attributes, attachments, input_attachments, output_attachments, descriptor_sets, stages_bytecode)
 {{
 }}
 
@@ -44,6 +48,7 @@ namespace RenderEngine
     {{
     public:
         {shader_name}() = delete;
+        {shader_name}(const {shader_name}& other) = delete;
         {shader_name}(const GPU* gpu);
         ~{shader_name}();
     }};
@@ -89,34 +94,51 @@ def _get_subpasses_code(shader_path: pathlib.Path) -> dict:
     return subpasses
 
 
-def _get_binding_descriptions(subpasses: dict) -> str:
+def _get_subpasses_layout(subpasses: dict) -> list[dict]:
     """
-    returns the description of all bindings
+    return for each subpass a dict describing the layout
     """
-    subpass_descriptors = []
-    for subpass, stages in subpasses.items():
-        bindings = {}
-        for stage in STAGES.keys():
-            src = stages.get(stage, {"source": ""})["source"]
-            matches = BINDING_REGEX.findall(src)
-            for (bset, bind, offset, btype, name, count) in matches:
-                bset = bset or "0"
-                descriptor: dict = bindings.get(bset, {})
-                _stages = descriptor.get(name, {"stages": set()})["stages"]
-                _stages = _stages | {STAGES[stage]}
-                descriptor[name] = {"binding": bind, "offset": offset or "0", "type": btype, "count": count or "1", "stages": _stages}
-                bindings[bset] = descriptor
-        # generate strings
+    subpasses_layouts = []
+    for subpass, stages_code in subpasses.items():
         descriptors = []
-        for set_index, set_descriptor in sorted(bindings.items()):  # for each bindings set
-            set_bindings = []
-            for name, props in set_descriptor.items():  # for each binding
-                type = TYPES.get(props["type"], "VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER")
-                VkDescriptorSetLayoutBinding = "{"+props["binding"]+", "+type+", "+props["count"]+", "+" | ".join(props["stages"])+", "+"nullptr"+"}"
-                set_bindings.append("{\""+name+"\", "+VkDescriptorSetLayoutBinding+"}")
-            descriptors.append("{"+", ".join(set_bindings)+"}")
-        subpass_descriptors.append("{"+", ".join(descriptors)+"}")
-    return "{"+", ".join(subpass_descriptors)+"}"
+        vertex_inputs = []
+        input_attachments = []
+        output_attachments = []
+        for extension, stage in STAGES.items():
+            src = stages_code.get(extension, {"source": ""})["source"]
+            matches = LAYOUT_REGEX.findall(src)
+            for (input_index, _set, binding, offset, location, storage, _type, name, count) in matches:
+                if binding != "" and storage in ("uniform", "buffer"):
+                    descriptors.append({"set": _set or "0", "binding": binding, "name": name, "count": count or "1", "stage": stage,
+                                        "type": UNIFORM_TYPES[_type] if storage == "uniform" else BUFFER_TYPE})
+                    if _type == "subpassInput":
+                        assert stage == "VK_SHADER_STAGE_FRAGMENT_BIT"
+                        input_attachments.append({"name": name, "input_index": input_index})
+                if stage == "VK_SHADER_STAGE_FRAGMENT_BIT" and storage == "out":
+                    output_attachments.append({"name": name, "type": _type})
+                if stage == "VK_SHADER_STAGE_VERTEX_BIT" and storage == "in":
+                    vertex_inputs.append({"name": name, "location": location, "binding": binding or "0", "type": _type, "offset": offset})
+        subpasses_layouts.append({"descriptors": descriptors, "vertex_inputs": vertex_inputs, "input_attachments": input_attachments, "output_attachments": output_attachments})
+    return subpasses_layouts
+
+
+def _nested(nested: Iterable) -> str:
+    """
+    recursively creates a string representation of the nested iterables
+
+    Example
+    -------
+    >>> _nested([('A', 'B'), [3, 4, 5, 6]])
+    "{{'A', 'B'}, {3, 4, 5, 6}}"
+    """
+    if isinstance(nested, str) and not nested.isdigit():
+        return f"\"{nested}\""
+    elif isinstance(nested, bytes):
+        return nested.decode()
+    elif isinstance(nested, str) or not hasattr(nested, "__iter__"):
+        return f"{nested}"
+    else:
+        return "{" + ", ".join(_nested(x) for x in nested) + "}"
 
 
 def constructor(shader_path: pathlib.Path):
@@ -124,17 +146,45 @@ def constructor(shader_path: pathlib.Path):
     generate C++ constructor source code for the given shader
     """
     subpasses = _get_subpasses_code(shader_path)
-    # set vertex inputs
-    binding_sets_descriptions = _get_binding_descriptions(subpasses)
-    vertex_inputs = ""
-    fragment_inputs = ""
-    fragment_outputs = ""
-    stages_bytecode = ""
+    layouts = _get_subpasses_layout(subpasses)
+    # order unique attachments :
+    # input attachment are identified by a name and an index, so we have to make sure they at at the correct index in the list of unique attachments
+    all_input_attachments = {att["name"]: int(att["input_index"]) for subpass in layouts for att in subpass["input_attachments"]}
+    att_types = {att["name"]: f"Type::{att['type'].upper()}".encode() for subpass in layouts for att in subpass["output_attachments"]}
+    att_indexes = {name: min((i, j) for i, subpass in enumerate(layouts) for j, att in enumerate(subpass["output_attachments"]) if att["name"] == name)
+                   for name in att_types.keys() if name not in all_input_attachments.keys()}  # non-input attachments by first (subpass, attachment) apparition indexes
+    attachments = [name for name, index in sorted(att_indexes.items(), key=lambda x: x[1])]
+    for name, index in sorted(all_input_attachments.items(), key=lambda x: x[1]):
+        attachments.insert(index, name)
+    for att in attachments:
+        if att not in att_indexes.keys():
+            raise RuntimeError(f"'{att}' is referenced as a subpass input attachment but not as an output attachment in the shader.")
+    attachments = _nested((name, att_types[name]) for name in attachments)
+    # list inputs attachments and output attachments at each stage
+    input_attachments = _nested((att["name"] for att in subpass["input_attachments"]) for subpass in layouts)
+    output_attachments = _nested((att["name"] for att in subpass["output_attachments"]) for subpass in layouts)
+    # for each stage, for each set, the list of descriptor sets
+    descriptors = _nested(({descriptor["set"]: [(desc["name"], (desc["binding"], desc["type"].encode(), desc["count"], desc["stage"].encode()))
+                                                for desc in subpass["descriptors"] if descriptor["set"] == desc["set"]]}.values()
+                           for descriptor in subpass["descriptors"]) for subpass in layouts)
+    # For each subpass the vertex attributes
+    bindings = [{vi["binding"]: vi["name"].split("_")[0] for vi in subpass["vertex_inputs"]} for subpass in layouts]
+    vertex_buffer_bindings = _nested([(name, (binding, f"sizeof({name})".encode(), "VK_VERTEX_INPUT_RATE_VERTEX".encode() if name == "Vertex" else "VK_VERTEX_INPUT_RATE_INSTANCE".encode()))
+                                      for binding, name in sorted(subpass.items())]
+                                     for subpass in bindings)
+    vertex_buffer_attributes = _nested([(vi["name"].split("_")[-1], (vi["location"], vi["binding"], f"static_cast<VkFormat>(Type::{vi['type'].upper()})".encode(), f"offsetof({vi['name'].split('_')[0]}, {vi['name'].split('_')[-1]})".encode()))
+                                        for vi in subpass["vertex_inputs"]]
+                                       for subpass in layouts)
+    # shader stage bytecodes
+    stages_bytecode = _nested([(STAGES[k].encode(), list(v["bytes"])) for k, v in subpass.items()] for subpass_name, subpass in subpasses.items())
+    # assembling and writing
     code = CONSTRUCTOR_SRC.format(shader_name=shader_path.name,
-                                  bindings_sets=binding_sets_descriptions,
-                                  vertex_inputs=vertex_inputs,
-                                  fragment_inputs=fragment_inputs,
-                                  fragment_outputs=fragment_outputs,
+                                  vertex_buffer_bindings=vertex_buffer_bindings,
+                                  vertex_buffer_attributes=vertex_buffer_attributes,
+                                  attachments=attachments,
+                                  input_attachments=input_attachments,
+                                  output_attachments=output_attachments,
+                                  descriptor_sets=descriptors,
                                   stages_bytecode=stages_bytecode)
     with open(shader_path.with_suffix(".cpp"), "w", encoding="utf-8") as f:
         f.write(code)
